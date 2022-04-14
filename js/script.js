4578c9f843a51ba404c6f87fb1caae3ce17fb38a
zaida-hammel <zaida-hammel@users.noreply.github.com>
2022-04-14 21:26:15 +0000

    Prettified Code!

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@
 //
 // This program accepts user input
 
-"use strict"
+"use strict";
 
 /**
  * Check servie worker.
@@ -11,21 +11,22 @@
 if (navigator.serviceWorker) {
   navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
     scope: "/ICS2O-PWA-Test/",
-  })
+  });
 }
 
 /**
  * This function calculates area of a triangle.
  */
-function calculate () {
+function calculate() {
   // input
-  const length = parseFloat(document.getElementById('length-of-pyramid').value)
-  const width = parseFloat(document.getElementById('width-of-pyramid').value)
-  const height = parseFloat(document.getElementById('height-of-pyramid').value)
+  const length = parseFloat(document.getElementById("length-of-pyramid").value);
+  const width = parseFloat(document.getElementById("width-of-pyramid").value);
+  const height = parseFloat(document.getElementById("height-of-pyramid").value);
 
   // process
-  const volume = (length * width * height) / 3
+  const volume = (length * width * height) / 3;
 
   // output
-  document.getElementById('volume').innerHTML = 'The volume is: ' + volume.toFixed(2) + ' mm³'
+  document.getElementById("volume").innerHTML =
+    "The volume is: " + volume.toFixed(2) + " mm³";
 }
