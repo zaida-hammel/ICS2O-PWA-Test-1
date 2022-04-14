935b13abe15db68f1e2f610a76b69730a1ae6dc3
Zaida Hammel <99354763+zaida-hammel@users.noreply.github.com>
2022-04-14 21:40:22 +0000

    added files

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@
 //
 // This program accepts user input
 
-"use strict"
+"use strict";
 
 /**
  * Check servie worker.
@@ -11,14 +11,15 @@
 if (navigator.serviceWorker) {
   navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
     scope: "/ICS2O-PWA-Test/",
-  })
+  });
 }
 
 /**
  * This function calculates the volume of a sphere.
  */
-function calculate () {
+function calculate() {
   // input
+<<<<<<< HEAD
   const radius = parseInt(document.getElementById("radius").value)
   // process
   const volume = 4/3 * Math.PI * Math.pow(radius, 3)
@@ -26,3 +27,16 @@ function calculate () {
   document.getElementById("volume").innerHTML =
     "The volume is: " + volume.toFixed(2) + ' cm³' 
 }
+=======
+  const length = parseFloat(document.getElementById("length-of-pyramid").value);
+  const width = parseFloat(document.getElementById("width-of-pyramid").value);
+  const height = parseFloat(document.getElementById("height-of-pyramid").value);
+
+  // process
+  const volume = (length * width * height) / 3;
+
+  // output
+  document.getElementById("volume").innerHTML =
+    "The volume is: " + volume.toFixed(2) + " mm³";
+}
+>>>>>>> origin/main
