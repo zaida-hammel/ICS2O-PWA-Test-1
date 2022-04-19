315cb0a3bd5a4f6e03b418321066aef7ac3ff0f3
Zaida Hammel <99354763+zaida-hammel@users.noreply.github.com>
2022-04-19 20:32:36 +0000

    edited code

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@
  * Check servie worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
+  navigator.serviceWorker.register("/ICS2O-PWA-Test-extra/sw.js", {
     scope: "/ICS2O-PWA-Test-extra/",
   })
 }
